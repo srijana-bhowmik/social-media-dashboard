6d15b768cb1098af7fb95d10f07c1a3d4162ea87
Srijana <bhowmiksrijana@gmail.com>
2026-07-06 09:27:03 +0530

    frontend production api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";     //importing axios library to make HTTP requests
 
 const API = axios.create({      //Creates a customized Axios object, every request automatically starts with: http://localhost:3000/api
-    baseURL: "http://localhost:3000/api"        // this is API instance
+    baseURL: "https://social-media-dashboard-cvh5.onrender.com/api"        // this is API instance
 });
 
 
