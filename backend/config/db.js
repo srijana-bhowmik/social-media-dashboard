4cf571da8a0249d57810ea3a39a3c29fd2c824ff
Srijana <bhowmiksrijana@gmail.com>
2026-08-17 10:19:25 +0530

    deployed mysql in aiven

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,10 @@ const db=mysql.createConnection({
     user:process.env.DB_USER,
     port:process.env.DB_PORT,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
+    database:process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect((err)=>{
